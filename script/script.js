f421a38ce507751085153e7242068dc9192d7d98
Erős István <eros@valahol>
2026-01-15 20:06:02 +0100

    OraVege

--- script/script.js
+++ script/script.js
@@ -1,1 +1,24 @@
 console.log("Helló")
+
+const cella= document.createElement("div")
+cella.classList.add("cella")
+
+document.body.appendChild(cella)
+cella.addEventListener("mousedown", kattintas)
+cella.addEventListener("contextmenu", (event)=>event.preventDefault())
+
+function kattintas(event){
+    console.log(event.button)
+
+    switch (event.button){
+        case 0:
+            this.style.background="pink"
+            break
+        case 2:
+            event.currentTarget.style.backgroundColor="white"
+            event.currentTarget.style.backgroundImage ="url('./img/flag.png')"
+            event.currentTarget.style.backgroundSize ="contain"
+
+            break
+    }
+}
